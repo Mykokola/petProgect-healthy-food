dbfba1ac1cf0006fb2bc5ace6ce9499b5478ebcb
Mykokola <j0kfrbr@gmail.com>
2023-05-20 11:49:47 -0500

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,8 @@ import { fetchFood } from './fetchfood';
 import { drawCuntFoodNutrition } from './drawCaloriesCaunt';
 import { fetchDish } from './fetchDishs';
 import { drawDishsList } from './drawDishsList';
-if(!(localStorage.getItem('RECIP_SAVE'))){
-localStorage.setItem('RECIP_SAVE', JSON.stringify({ recipList: [] }));
+if (!localStorage.getItem('RECIP_SAVE')) {
+  localStorage.setItem('RECIP_SAVE', JSON.stringify({ recipList: [] }));
 }
 const cauntFoodBtn = document.querySelector('.count-calories-btn'),
   cauntCaloriesResult = document.querySelector('.caunt-calories-result'),
@@ -14,7 +14,7 @@ const cauntFoodBtn = document.querySelector('.count-calories-btn'),
 let saveRecips = [];
 if (saveRecipLocal) {
   saveRecips = [...saveRecipLocal];
-  console.log(saveRecips)
+  console.log(saveRecips);
 }
 
 cauntFoodBtn.addEventListener('click', e => {
@@ -45,11 +45,11 @@ choiceFoddBtn.addEventListener('click', e => {
       choiceFoodList.addEventListener('click', e => {
         e.preventDefault();
         let elemDish = e.target.parentNode.parentNode.outerHTML;
-       elemDish =  elemDish.replace(
+        elemDish = elemDish.replace(
           '<button type="button" class="choice-food-item--save-recipe-btn">Save the recipe</button>',
           ''
         );
-        if (e.target.type === 'button' && titleChecker(elemDish,saveRecips)) {
+        if (e.target.type === 'button' && titleChecker(elemDish, saveRecips)) {
           saveRecips.push(elemDish);
         }
         localStorage.setItem(
@@ -62,16 +62,19 @@ choiceFoddBtn.addEventListener('click', e => {
       console.log(e.message);
     });
 });
-function titleCreator(title){
-  let titleGenerator = title.split(' ')
-   return titleGenerator.slice(titleGenerator.indexOf('<h3'),titleGenerator.indexOf('</h3>\n')).join(' ').replace('<h3 class="choice-food-item--title">',``)
+function titleCreator(title) {
+  let titleGenerator = title.split(' ');
+  return titleGenerator
+    .slice(titleGenerator.indexOf('<h3'), titleGenerator.indexOf('</h3>\n'))
+    .join(' ')
+    .replace('<h3 class="choice-food-item--title">', ``);
 }
-function titleChecker(elemDish,titleMass){
-  elemDish = titleCreator(elemDish)
-  for(let i = 0;i < titleMass.length;i++){
-    if(titleCreator(titleMass[i]) === elemDish){
-      return 0
+function titleChecker(elemDish, titleMass) {
+  elemDish = titleCreator(elemDish);
+  for (let i = 0; i < titleMass.length; i++) {
+    if (titleCreator(titleMass[i]) === elemDish) {
+      return 0;
     }
   }
-  return 1
+  return 1;
 }
