e7e5e1929654dbc5b45a90fab1e5d99e3a1da3a4
Mykokola <119622278+Mykokola@users.noreply.github.com>
2023-05-20 02:12:45 +0000

    Deploying to gh-pages from @ Mykokola/petProgect-healthy-food@c23a5212a25fbc309b8f70e877459b3092a222f2 🚀

---
+++ cartRecep.931fb42f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".recip-list"),t=JSON.parse(localStorage.getItem("RECIP_SAVE"));t.recipList.length&&(e.innerHTML="",e.insertAdjacentHTML("beforeend",t.recipList.join("")));
+//# sourceMappingURL=cartRecep.931fb42f.js.map
---
+++ cartRecep.e4a24ff1.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".recip-list"),t=JSON.parse(localStorage.getItem("RECIP_SAVE"));t.recipList.length&&(e.innerHTML="",e.insertAdjacentHTML("beforeend",t.recipList.join("")))}();
+//# sourceMappingURL=cartRecep.e4a24ff1.js.map
